72f4638738162ceed5b243cf98d8a5c8604c99b7
Cuper99 <binzariion@gmail.com>
2023-03-03 22:52:33 +0200

    first commit

---
+++ mobile-app/config/AppConfig.js
@@ -0,0 +1,8 @@
+module.exports.AppConfig = {
+        app_name: 'Intercar', 
+        app_description: 'Serios', 
+        app_identifier: 'com.exicube.taxi', 
+        ios_app_version: '1.0.0', 
+        android_app_version: 100, 
+        facebookAppId: '12345', 
+    }
---
+++ mobile-app/config/FirebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports.FirebaseConfig = {
+	"projectId": "aplicatiemobila-c685c",
+	"appId": "1:552632438314:web:6bf70ec9e851332ea57d53",
+	"databaseURL": "https://aplicatiemobila-c685c-default-rtdb.europe-west1.firebasedatabase.app",
+	"storageBucket": "aplicatiemobila-c685c.appspot.com",
+	"apiKey": "AIzaSyALGSQ8fI74BVxtJgH-YvwmOIPgU515kt0",
+	"authDomain": "aplicatiemobila-c685c.firebaseapp.com",
+	"messagingSenderId": "552632438314",
+	"measurementId": "G-2EJ1BYNJEC"
+};
